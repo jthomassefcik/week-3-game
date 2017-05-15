3eb26c11f67ab37ebfd8107831b90ba1e300e0c3
Joseph Sefcik <josephsefcik@Josephs-MacBook-Pro.local>
2017-05-15 09:13:14 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,39 @@
+$(document).ready(resetHangman)
+var answerWord = "";
+var displayedAnswer = "";
+
+
+
+
 function chooseWord() {
     var words = ["fireworks", "hotdog", "hamburger", "flyover", "freedom"];
     return words[Math.floor(Math.random() * words.length)];
 }
+
+function blanksforAnswer(answerWord) {
+    var answerWord = answerWord
+    var blanks = "";
+    for (var i = 0; i < answerWord.length; i++) {
+        blanks = blanks + "_";
+    }
+    
+}
+
+function resetHangman() {
+    answerWord = chooseWord();
+    displayedAnswer = blanksforAnswer(answerWord);
+    $("<div>").html(blanks).appendTo($("#current-word"));
+}
         
 
 
-        
+
+
+})
+
+
+
+
 
 
 
